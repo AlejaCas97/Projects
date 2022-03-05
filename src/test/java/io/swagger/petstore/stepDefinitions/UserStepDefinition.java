91acac9e31550a7c17231cdeb6880256c37fae16
SQABOG-SQA2022\SQA <paula.castaneda@sqasa.co>
2022-03-05 11:16:30 -0500

    post, get y delete de user y pet

---
+++ src/test/java/io/swagger/petstore/stepDefinitions/UserStepDefinition.java
@@ -0,0 +1,32 @@
+package io.swagger.petstore.stepDefinitions;
+
+
+import cucumber.api.java.es.*;
+import io.swagger.petstore.steps.PetstoreSteps;
+import net.thucydides.core.annotations.Steps;
+
+public class UserStepDefinition {
+
+    @Steps
+    PetstoreSteps petstoreSteps = new PetstoreSteps();
+
+    /*No es necesario el metodo Dado o Given, porque para automatizar servicios
+     *no se requiere levantar un navegador web
+     */
+
+    @Cuando("^creo y consulto un usuario$")
+    public void creoYConsultoUnaOrden() {
+        petstoreSteps.consultarUsuario();
+        petstoreSteps.crearUsuario();
+        petstoreSteps.eliminarUsuario();
+        petstoreSteps.consultarUsuario();
+
+    }
+
+    @Entonces("^luego elimino$")
+    public void luegoElimino() {
+
+    }
+
+
+}
